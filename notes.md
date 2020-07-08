6cba14f7a8ecd8c1a1358fe052acdff240c4971a
Jeffe <j.garner200@gmail.com>
2020-07-08 11:55:38 -0400

    firs commit

---
+++ notes.md
@@ -0,0 +1,9 @@
+About Redux
+
+- A third-party state management library. There is an additional library to make it work with React, but it is intended to work for many runtime environments.
+
+Step 1. Provider
+
+Step 2. Store, Middleware
+
+Step 3. Reducers, Action Creators
--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+
+const App = () => {
+	return <div></div>;
+};
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+	<Provider store={createStoreWithMiddleware()}>
+		<App />
+	</Provider>,
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+	document.getElementById("root")
+);
